5b4d849f94be4fd68d554bd79f4d5ad025f168c6
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-08-11 16:48:31 +0530

    Now displays burger and its ingredients

--- src/Components/Burger/Burger.js
+++ src/Components/Burger/Burger.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import classes from './burger.module.css';
+import Ingridients from './Ingridients/Ingridients';
 
-class Burger{
-    render(){
-        return (
-            <div/>
-        );
-    };
+const Burger = (props) =>{
+
+    let transIngrid = Object.keys(props.ingridients).map(igKey=>{
+        return [...Array(props.ingridients[igKey])].map((_, i) => {
+            return <Ingridients key={igKey + i} type={igKey}/>;
+        })
+    }).reduce((arr, el)=>{
+        return arr.concat(el)
+    }, []);
+
+    if(transIngrid.length === 0){
+        transIngrid = <div className="alert alert-warning">Please start adding some ingridients</div>
+    }
+
+    return (
+        <div className={classes.burger}>
+            <Ingridients type="bread-top"/>
+            {transIngrid}
+            <Ingridients type="bread-bottom"/>
+        </div>
+    );
 }
 
 export default Burger;
