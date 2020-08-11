590b595dad5920cff341eea6338af8ee7291c6e8
Z3T4Z00K <agarwal.archit17@gmail.com>
2020-08-11 12:24:24 +0530

    Added Burger Builder

---
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+
+class BurgerBuilder extends Component{
+    render(){
+        return(
+            <Fragment>
+                <div className="min-h-100 dis-flex flex-col container-fluid">
+                    <div className="container">
+
+                    </div>
+                </div>
+            </Fragment>
+        );
+    }
+}
+
+export default BurgerBuilder;
